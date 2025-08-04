553448d545b2797da75bad3e889e4d0d2e34c482
Senethma Hewavitharana <senethmahewavitharana@gmail.com>
2025-08-04 09:05:17 +0530

    Adding a new screen to show up once you click start

--- src/ClosetGUI.java
+++ src/ClosetGUI.java
@@ -1,5 +1,6 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import  javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -20,9 +21,28 @@ public class ClosetGUI extends Application{
         primaryStage.setTitle("Closet Organizer");
         primaryStage.setScene(scene);
         primaryStage.show();
+
+        startButton.setOnAction(e -> showOrganizer(primaryStage));
     }
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public void showOrganizer(Stage stage){
+        Label typeLabel = new Label("Clothing type: ");
+        TextField typeInput = new TextField();
+
+        Label nameLabel = new Label("Clothing name: ");
+        TextField nameInput = new TextField();
+
+        Button addButton = new Button("Add to Closet");
+
+        VBox layout = new VBox(15);
+        layout.getChildren().addAll(typeLabel,typeInput,nameLabel,nameInput,addButton);
+
+        Scene organizerScene = new Scene(layout,400,200);
+        stage.setTitle("Organizer Closet");
+        stage.setScene(organizerScene);
     }
 }
